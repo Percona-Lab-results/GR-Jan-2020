08c37055a791d6c770c3d44c009a7fce56ee7825
root <you@example.com>
2020-01-24 16:21:10 +0000

    GR results

--- run.sh
+++ run.sh
@@ -1,12 +1,13 @@
-RUNDIR=res-tpcc-PXC-slv1-BP100-`date +%F-%H-%M`
+RUNDIR=res-tpcc-GR-slv64-opt-EVENTUAL-`date +%F-%H-%M`
 runid="1"
+#for i in 16 64 128 256
 for i in 1 2 4 8 16 64 128 256
 do
      	OUTDIR=$RUNDIR/$runid/thr$i
         mkdir -p $OUTDIR
 
         time=3600
-	./tpcc.lua --mysql-host=139.178.88.121 --mysql-user=sbtest --mysql-password=sbtest --mysql-db=sbtest --time=3600 --threads=$i --report-interval=1 --tables=100 --scale=10 --trx_level=RR  --db-driver=mysql --report_csv=yes run |  tee -a $OUTDIR/res.txt
+	./tpcc.lua --mysql-host=139.178.88.121 --mysql-user=sbtest --mysql-password=sbtest --mysql-db=sbtest --time=3600 --threads=$i --report-interval=1 --tables=100 --scale=10 --trx_level=RR  --db-driver=mysql --report_csv=yes --mysql-ignore-errors=3100,3101,1213,1180 run |  tee -a $OUTDIR/res.txt
 
 	sleep 30
 	done
